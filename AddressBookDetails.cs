9cf64a1e6fc579210aae4bcc107615f916dcd145
Bhagi <puppyreddy1997@gmail.com>
2020-11-24 20:24:07 +0530

    [Bhagyalaxmi]ADDED:Added code for Ability to add a new person into AddressBook

---
+++ AddressBookDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBookDetails
+    {
+        public String firstName;
+        public String lastName;
+        public String address;
+        public String city;
+        public String state;
+        public long phoneNumber;
+        public long zip;
+        List<ContactPerson> personDetails = new List<ContactPerson>();
+
+
+        public void addPersonDetails()
+        {
+            Console.WriteLine("Enter person Details :");
+            Console.WriteLine("\n Enter FirstName : ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("\n Enter LastName : ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("\n Enter Address : ");
+            address = Console.ReadLine();
+            Console.WriteLine("\n Enter City : ");
+            city = Console.ReadLine();
+            Console.WriteLine("\n Enter State : ");
+            state = Console.ReadLine();
+            Console.WriteLine("\n Enter MobileNumber : ");
+            phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("\n Enter Zip : ");
+            zip = long.Parse(Console.ReadLine());
+
+            personDetails.Add(new ContactPerson(firstName, lastName,address, city, state, phoneNumber, zip));
+            foreach (ContactPerson addPerson in personDetails)
+                Console.WriteLine(addPerson.toString());
+            Console.WriteLine("Person Details  Added Successfully \n");
+        }
+    }
+}
--- ContactPerson.cs
+++ ContactPerson.cs
@@ -6,14 +6,6 @@ namespace AddressBook
 {
     class ContactPerson
     {
-        public String firstName;
-        public String lastName;
-        public String address;
-        public String city;
-        public String state;
-        public long phoneNumber;
-        public long zip;
-
         public ContactPerson(String firstName, String lastName, String address, String city, String state, long phoneNumber, long zip)
         {
             this.firstName = firstName;
@@ -23,6 +15,22 @@ namespace AddressBook
             this.state = state;
             this.phoneNumber = phoneNumber;
             this.zip = zip;
+        }
+        public String firstName
+        {
+            get;
+            set;
+        }
+        public String lastName { get; set; }
+        public String address { get; set; }
+        public String city { get; set;}
+        public String state { get; set; }
+        public long phoneNumber { get; set; }
+        public long zip { get; set; }
+
+        public string toString()
+        {
+            return "Name:" + firstName + " " + lastName + "\t Address:" + address + "\t City:" + city + "\t State:" + state + "\t MobileNumber" + phoneNumber + "\t PinCode" + zip;
         }
     }
 }
